1eaea3671e3c93212d1a5d7c5acd68cc94a332d9
Kareem El-Faramawi <kareem02@gmail.com>
2016-05-03 03:35:40 -0400

    Add walkable helper and backtrace

--- PathFinder/Util.cs
+++ PathFinder/Util.cs
@@ -9,5 +9,20 @@ namespace PathFinder {
         public static bool IsValid(int x, int y, Grid grid) {
             return 0 <= x && x < grid.Width && 0 <= y && y < grid.Height;
         }
+
+        public static bool IsWalkableAt(int x, int y, Grid grid) {
+            return IsValid(x, y, grid) && grid[y, x].IsWalkable;
+        }
+
+        public static List<Node> backtrace(Node node) {
+            List<Node> path = new List<Node> { node };
+            while (node.Parent != null) {
+                path.Add(node.Parent);
+                node = node.Parent;
+            }
+            path.Reverse();
+            return path;
+        }
+
     }
 }
